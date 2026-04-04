aff4605496bedba1d9b747430d5e572731087fc1
Jono Mingard <reason.koan@gmail.com>
2026-04-04 23:08:37 +0100

    Bump version

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "Gumroad",
   slug: "gumroad",
-  version: "2026.03.31",
+  version: "2026.04.04",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "gumroadmobile",
